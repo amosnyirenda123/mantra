024ff9352a12d3961eac276ab66e8f3281aacc04
Amos Nyirenda <nyirendaamos1@gmail.com>
2026-07-13 14:28:20 +0100

    first commit

---
+++ src/app_sup.erl
@@ -0,0 +1,36 @@
+%%% @author Nyirenda Amos <nyirendaamos1@gmail.com>
+%%% @copyright (C) 2026, Nyirenda Amos
+%%% @doc 
+%%%
+%%% @end
+%%% Created : 13 Jul 2026 by Nyirenda Amos <nyirendaamos1@gmail.com>
+-module(app_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+-export([init/1]).
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init(_Args) ->
+    SupervisorSpecification = #{
+        strategy => one_for_one, % one_for_one | one_for_all | rest_for_one | simple_one_for_one
+        intensity => 10,
+        period => 60},
+
+    ChildSpecifications = [
+        #{
+            id => A_Name,
+            start => {A_Worker_Module, start_link, []},
+            restart => permanent, % permanent | transient | temporary
+            shutdown => 2000, % use 'infinity' for supervisor child
+            type => worker, % worker | supervisor
+            modules => [A_Worker_Module]
+        }
+    ],
+
+    {ok, {SupervisorSpecification, ChildSpecifications}}.
+
